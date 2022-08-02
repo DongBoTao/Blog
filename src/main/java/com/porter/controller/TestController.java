86736ea2ea9ff37e12e8b0b0ad56769f7e93f1f3
Porter <dbt418@sina.cn>
2022-08-02 18:24:22 +0800

    创建项目内容

---
+++ src/main/java/com/porter/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.porter.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: Porter
+ * @create: 2022/8/2 18:23
+ * @Description:
+ * @FileName: TestController
+ * @History:
+ */
+@RestController
+public class TestController {
+}
